816d91029f767ca77460ed6dae1c040aa9852944
Benjamin TEGGE <benjaminosm@googlemail.com>
2024-07-08 01:33:42 +0200

    Fix convenience script

--- hack/autoinstall.sh
+++ hack/autoinstall.sh
@@ -22,7 +22,7 @@ detect_package_suffix() {
 # Function to get the latest release tag from GitHub
 get_latest_release() {
     local repo="$1"
-    curl -s "https://api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+    curl -Ls "https://api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
 
 github_repo="uptime-lab/computeblade-agent"
